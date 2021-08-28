cf4a8f3d80d6adce89e9182bd29c231e643a10a9
Bhakti199 <78597483+Bhakti199@users.noreply.github.com>
2021-08-28 12:27:46 +0530

    fetch API

--- app.js
+++ app.js
@@ -1,9 +1,31 @@
-var btnTranslate= document.querySelector("#btn-translate");
+var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
-console.log(txtInput)
-    function clickHandler (){
-        console.log("clicked");
-        console.log("input", txtInput.value);
-    };
-btnTranslate.addEventListener("click",clickHandler)
-  
+var outputDiv = document.querySelector("#output");
+
+var serverUrl = "https://api.funtranslations.com/translate/minion.json"
+// var serverUrl ="https://api.funtranslations.com/translate/pirate.json"
+
+function getTranslationUrl(input) {
+    return serverUrl + "?" + "text=" + input
+}
+
+function errorHandler(error) {
+    console.log("error occured", error);
+    alert("something wrong with server! try again after some time")
+}
+
+function clickHandler() {
+
+    var inputText = txtInput.value;
+
+    fetch(getTranslationUrl(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText;
+
+        })
+    .catch(errorHandler)
+};
+
+btnTranslate.addEventListener("click", clickHandler)
